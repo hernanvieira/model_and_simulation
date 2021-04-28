95f385dc128ff2a461f3383264a7e5eb490f5421
hernanvieira <hernanvieira199@gmail.com>
2021-04-28 17:14:12 -0300

    Imports corregidos

--- src/components/Indicacion.js
+++ src/components/Indicacion.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 
 class Indicacion extends Component {
-    render(){
-        return(
-            <p class="text-muted card-title" >Para utilizar los generadores de números pseudoaleatorios basta con
-            seleccionar el método a utilizar y completar los campos solicitados por el método. Luego de cargar
-            los datos solicitados, presionar en el botón <b>Generar.</b></p>
-        );
-    }
+  render() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item sm={12} lg={6} xs={12}>
+          <Typography variant="subtitle1">
+            Para utilizar los generadores de números pseudoaleatorios basta con
+            seleccionar el método a utilizar y completar los campos solicitados
+            por el método. Luego de cargar los datos solicitados, presionar en
+            el botón <b>Generar.</b>
+          </Typography>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default Indicacion;
